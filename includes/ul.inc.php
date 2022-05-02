3978f00bebfa87abb497cd7e2445f01730205a53
BOYER62 <boyer.olivier2@free.fr>
2022-05-02 12:01:14 +0200

    Fin_premier_formulaire

--- includes/ul.inc.php
+++ includes/ul.inc.php
@@ -1,13 +1,13 @@
 <nav>
     
 <div class="list-group">
-  <a href="index.php?debogage" class="list-group-item list-group-item-action" data-bs-toggle="button" autocomplete="off">
+  <a href="index.php?debogage" class="btn btn-outline-primary w-100" data-bs-toggle="button" autocomplete="off">
     Debogage
   </a>
-  <a href="index.php?concatenation" class="list-group-item list-group-item-action" data-bs-toggle="button" autocomplete="off" aria-pressed="true">Concaténation</a>
-  <a href="index.php?boucle" role="button" class="list-group-item list-group-item-action">Boucle</a>
-  <a href="index.php?function" role="button" class="list-group-item list-group-item-action">Fonction</a>
-  <a href="index.php?supprimer" role="button" class="list-group-item list-group-item-action">Supprimer</a>
+  <a href="index.php?concatenation" class="btn btn-outline-primary w-100" data-bs-toggle="button" autocomplete="off" aria-pressed="true">Concaténation</a>
+  <a href="index.php?boucle" role="button" class="btn btn-outline-primary w-100">Boucle</a>
+  <a href="index.php?function" role="button" class="btn btn-outline-primary w-100">Fonction</a>
+  <a href="index.php?supprimer" role="button" class="btn btn-outline-primary w-100">Supprimer</a>
 </div>
    
 </nav>
